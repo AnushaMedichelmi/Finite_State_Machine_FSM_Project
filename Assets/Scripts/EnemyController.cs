8388e334c67ba9fab4887c01ebbf709aa1b90b14
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-04-18 16:25:01 +0530

    EnemyController script is added

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+   public enum STATE { LOOKFOR,GOTO,ATTACK}
+    public STATE currentState=STATE.LOOKFOR;
+    public float enemySpeed;
+    public float goToDistance;   
+    public float attackDistance;
+    public Transform target;
+    public string playerTag;
+
     // Start is called before the first frame update
-    void Start()
+  /* private  void Start()
     {
-        
-    }
+        StartCoroutine("Starts");
+    }*/
+
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Starts()
+    {
+        while(true)
+        {
+           switch(currentState)
+            {
+                case STATE.LOOKFOR:
+                    LookFor();
+                    break;
+                    case STATE.GOTO:
+                    GOTO();
+                    break;
+                case STATE.ATTACK:
+                    ATTACK();
+                    break;
+
+                default:
+                    break;
+            }
+            yield return 0;
+        }
+       
+    }
+
+    public void LookFor()
+    {
+        if(Vector3.Distance(transform.position, target.position) < goToDistance)
+        {
+            currentState = STATE.GOTO;
+        }
+        Debug.Log("This a LOOKFOR Method");
+    }
+
+    public void GOTO()
+    {
+        Debug.Log("GOTO METHOD");
+    }
+
+    public void ATTACK()
+    {
+        Debug.Log("ATTACK METHOD");
     }
 }
